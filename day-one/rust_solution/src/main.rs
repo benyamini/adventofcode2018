337e88ba1bfbb4ce93f428beab6d541d3712ac79
Ben McGarvey <ben@sidekicker.com.au>
2018-12-21 15:12:44 +1300

    refactor

--- day-one/rust_solution/src/main.rs
+++ day-one/rust_solution/src/main.rs
@@ -3,11 +3,32 @@ use std::collections::HashMap;
 
 fn main() {
     let contents = parse_input("inputs".to_string());
+    let (total, double) = solve(contents);
+
+    match total {
+        Some(x) => println!("Total: {}", x),
+        None => (),
+    };
+
+    match double {
+        Some(x) => println!("Found twice: {}", x),
+        None => (),
+    };
+}
+
+fn parse_input(filename: String) -> String {
+    fs::read_to_string(filename)
+        .unwrap()
+}
+
+fn solve(contents: String) -> (Option<i32>, Option<i32>) {
     let mut counted: bool = false;
     let mut count: i32 = 0;
+    let mut final_count: Option<i32> = None;
 
     let mut values: HashMap<i32, i32> = HashMap::new();
     let mut twice_counted: bool = false;
+    let mut twice_counted_value: Option<i32> = None;
 
     while !twice_counted {
         for line in contents.split('\n') {
@@ -23,19 +44,16 @@ fn main() {
                 *value_count += 1;
                 if *value_count == 2 {
                     twice_counted = true;
-                    println!("{}", count);
+                    twice_counted_value = Some(count);
                     break;
                 }
             }
         }
         if !counted {
-            println!("{}", count);
             counted = true;
+            final_count = Some(count);
         }
     }
-}
 
-fn parse_input(filename: String) -> String {
-    fs::read_to_string(filename)
-        .unwrap()
+    (final_count, twice_counted_value)
 }
